6834f79fdca489868d3cdc38a5ef673572b8fb8b
Heryck <heryckmota@gmail.com>
2022-12-21 12:16:37 -0300

    first commit

---
+++ app/Http/Controllers/PessoaController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PessoaController extends Controller
+{
+    public function storePessoa(Request $request)
+    {
+        try {
+        } catch(\Exception $e) {
+        }
+    }
+}
